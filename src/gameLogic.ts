a34fd4af1c75850ac08de13fa76791fa13e8ae60
wai-lin <dev.wailin@gmail.com>
2026-02-28 20:14:15 +0700

    feat: simple wordle game logic with game state

---
+++ src/gameLogic.ts
@@ -0,0 +1,91 @@
+export type GameStatus = "won" | "failed" | "playing"
+export type TileStatus = "wrong" | "correct" | "incorrect_place"
+export interface GameTileRecord {
+	value: string
+	status: TileStatus
+}
+export type GameHistory = Array<GameTileRecord[]>
+export type GameState = {
+	correctAns: string
+	status: GameStatus
+	history: GameHistory
+}
+
+const MAX_GUESS_CHANCE = 6
+
+function checkTile(
+	correctAns: string,
+	tileVal: string,
+	tilePos: number,
+): TileStatus {
+	const hasChar = correctAns.includes(tileVal)
+	if (hasChar && correctAns[tilePos] !== tileVal) return "incorrect_place"
+	else if (hasChar && correctAns[tilePos] === tileVal) return "correct"
+	return "wrong"
+}
+
+function hasCorrectAns(history: GameHistory) {
+	return history.some((row) => {
+		return row.every((tile) => tile.status === "correct")
+	})
+}
+
+export function initGameState(correctAns: string): GameState {
+	return {
+		correctAns,
+		status: "playing",
+		history: Array(MAX_GUESS_CHANCE)
+			.fill(null)
+			.map(() =>
+				Array(5)
+					.fill(null)
+					.map(() => ({ value: "_", status: "wrong" })),
+			),
+	}
+}
+
+export interface CheckAnswerFnArgs {
+	correctAns: string
+	history: GameHistory
+	tileVal: string
+	tilePos: number
+}
+export function checkAnswer({
+	correctAns,
+	history,
+	tileVal,
+	tilePos,
+}: CheckAnswerFnArgs): GameState {
+	if (hasCorrectAns(history))
+		return {
+			correctAns,
+			history,
+			status: "won",
+		}
+
+	const tmpHistory = [...history]
+
+	let histRow = history.findIndex((row) => row.find((t) => t.value === "_"))
+	let nextTilePos = tmpHistory[histRow].findIndex((t) => t.value === "_")
+	// move to new row if nextTilePos exceed the 5 char count
+	if (nextTilePos >= 5) {
+		histRow += 1
+		nextTilePos = 0
+	}
+
+	const titleStatus = checkTile(correctAns, tileVal, tilePos)
+	tmpHistory[histRow][nextTilePos] = { value: tileVal, status: titleStatus }
+
+	if (hasCorrectAns(tmpHistory))
+		return {
+			correctAns,
+			history: tmpHistory,
+			status: "won",
+		}
+
+	return {
+		correctAns,
+		history: tmpHistory,
+		status: "playing",
+	}
+}
