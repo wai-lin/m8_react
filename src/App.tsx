0ba12bed59de82b13b4bd08640b24af7e8c7e1b0
wai-lin <dev.wailin@gmail.com>
2026-03-02 19:21:26 +0700

    feat: add global key listener and game result dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,70 @@
-import { useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { Guesses } from "./Guesses"
 import { Keyboard } from "./Keyboard"
+import { ALLOWED_KEYS } from "./constants"
 import { checkAnswer, initGameState } from "./gameLogic"
 
 const WORD_OF_THE_DAY = "logic"
 
 export function App() {
+	const gameResultRef = useRef<HTMLDialogElement>(null)
 	const [answer, setAnswer] = useState("")
 	const [gameState, setGameState] = useState(initGameState(WORD_OF_THE_DAY))
 
-	const handleOnKeyPress = (key: string) => {
-		if (gameState.status !== "playing") return
+	useEffect(() => {
+		if (gameState.status !== "playing") {
+			gameResultRef.current?.show()
+		}
+	}, [gameState.status])
 
-		const newAnswer = (answer.length >= 5 ? "" : answer) + key
-		const newGameState = checkAnswer({
-			correctAns: WORD_OF_THE_DAY,
-			history: gameState.history,
-			tileVal: key,
-			tilePos: Math.max(0, newAnswer.length - 1),
-		})
+	const handleOnKeyPress = useCallback(
+		(key: string) => {
+			if (gameState.status !== "playing") return
 
-		console.log(newGameState)
+			const newAnswer = (answer.length >= 5 ? "" : answer) + key
+			const newGameState = checkAnswer({
+				correctAns: WORD_OF_THE_DAY,
+				history: gameState.history,
+				tileVal: key,
+				tilePos: Math.max(0, newAnswer.length - 1),
+			})
 
-		if (newGameState.status === "won") {
+			if (newGameState.status === "won") {
+				setGameState(newGameState)
+				return
+			}
+
 			setGameState(newGameState)
-			return
+			setAnswer(newAnswer)
+		},
+		[answer, gameState.history, gameState.status],
+	)
+
+	useEffect(() => {
+		const handler = (ev: KeyboardEvent) => {
+			const key = ev.key
+			if (ALLOWED_KEYS.includes(key) && gameState.status === "playing")
+				handleOnKeyPress(key)
 		}
 
-		setGameState(newGameState)
-		setAnswer(newAnswer)
-	}
+		window.addEventListener("keyup", handler)
+		return () => {
+			window.removeEventListener("keyup", handler)
+		}
+	}, [gameState.status, handleOnKeyPress])
 
 	return (
-		<>
+		<main>
 			<h1 style={{ textAlign: "center" }}>Wordle</h1>
 			<Guesses gameHistory={gameState.history} />
 			<div style={{ height: "2rem" }} />
 			<Keyboard onKeyPress={handleOnKeyPress} />
-		</>
+			<dialog ref={gameResultRef}>
+				{gameState.status === "won" && <h1>Yay.. you won! 🎉</h1>}
+				{gameState.status === "failed" && (
+					<h1>You failed 🥲 the answer is {WORD_OF_THE_DAY}</h1>
+				)}
+			</dialog>
+		</main>
 	)
 }
