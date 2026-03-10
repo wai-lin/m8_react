2b9bca79150e3b0e43ea1e2e98a9cd9db9745998
wai-lin <dev.wailin@gmail.com>
2026-03-10 21:37:10 +0700

    refactor: base url to adapt depending on the build env

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import { AppLayout } from "./AppLayout"
 import { Home } from "./Home"
 import { Leaderboard } from "./Leaderboard"
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			element: <AppLayout />,
+			children: [
+				{ index: true, element: <Home /> },
+				{ path: "play", element: <Home /> },
+				{ path: "leaderboard", element: <Leaderboard /> },
+			],
+		},
+	],
 	{
-		element: <AppLayout />,
-		children: [
-			{ index: true, element: <Home /> },
-			{ path: "play", element: <Home /> },
-			{ path: "leaderboard", element: <Leaderboard /> },
-		],
+		basename: import.meta.env.BASE_URL,
 	},
-])
+)
 
 export function App() {
 	return <RouterProvider router={router} />
